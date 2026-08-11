aeb908370e80ab843c1359339ffe1dd810790d48
ScryLk <lucaskepler991@gmail.com>
2026-08-11 19:35:33 -0300

    RIbbon load balance

--- hr-worker/src/main/java/org/example/resources/WorkerResource.java
+++ hr-worker/src/main/java/org/example/resources/WorkerResource.java
@@ -1,22 +1,29 @@
 package org.example.resources;
 
 
-import antlr.collections.impl.LList;
-import org.apache.coyote.Response;
 import org.example.entities.Worker;
 import org.example.repositories.WorkerRepository;
+import org.slf4j.LoggerFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.core.env.Environment;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 import java.util.List;
 
 @RestController
 @RequestMapping(value = "/workers")
 public class WorkerResource{
+
+        private static Logger logger = LoggerFactory.getLogger(WorkerResource.class);
+
+        @Autowired
+        private Environment env;
 
         @Autowired
         private WorkerRepository  repository;
@@ -29,6 +36,9 @@ public class WorkerResource{
 
         @GetMapping(value = "/{id}")
         public ResponseEntity<Worker> findById(@PathVariable Long id) {
+
+            logger.info("PORT = " + env.getProperty("local.server.port"));
+
             Worker obj = repository.findById(id).get();
             return ResponseEntity.ok(obj);
 
